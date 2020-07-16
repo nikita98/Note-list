ba6814db81e2f72311a967907529a35665ad7ddc
Nikita Zuev <nikita_zuev_98@mail.ru>
2020-07-16 22:08:27 +0800

    Store + visual

---
+++ src/views/todo-categories.vue
@@ -0,0 +1,36 @@
+<template>
+  <application-window title="Задачи">
+    <todo-category
+      v-for="todoCategory in TODOCATEGORIES"
+      :key="todoCategory.number"
+      :number="todoCategory.number"
+      :title="todoCategory.title"
+      :todos="todoCategory.todos"
+    />
+    <add label="Add category" v-on:addElem="addCategory"></add>
+  </application-window>
+</template>
+
+<script>
+import todoCategory from "@/components/todo-category.vue";
+import applicationWindow from "@/components/application-window.vue";
+import add from "@/components/add.vue";
+import { mapGetters } from "vuex";
+
+export default {
+  components: {
+    todoCategory,
+    applicationWindow,
+    add
+  },
+  methods: {
+    addCategory() {
+      
+    }
+  },
+  computed: {
+    //computed
+    ...mapGetters(["TODOCATEGORIES"])
+  }
+};
+</script>
